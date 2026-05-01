9b83b82b69786ea1c86005c333d63f9ab9f48d2c
Helton Heleno <helton_heleno@hotmail.com>
2026-04-30 22:41:39 -0300

    first commit

---
+++ src/2. JavaScript Basico - para iniciantes/aula1/7. console.log.js
@@ -0,0 +1,5 @@
+console.log('Helton Heleno'); // String
+console.log("Helton Heleno");
+console.log(`Helton Heleno`);
+
+console.log(35, 15.85, 'Helton Heleno');
---
+++ src/2. JavaScript Basico - para iniciantes/aula4/11. Comentários de código.js
@@ -0,0 +1,15 @@
+// E escrever um comentário
+console.log('Olá Mundo'); // aqui tem um comentário
+console.log('Linha 1');
+// Comentario
+
+/*
+*
+* um titulo 
+*
+*lorem  
+*/
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado
