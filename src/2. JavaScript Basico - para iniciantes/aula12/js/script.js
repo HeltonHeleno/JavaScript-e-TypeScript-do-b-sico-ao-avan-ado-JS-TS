2e935afdae2b9e9217e288fc9772450de1a87a66
Helton Heleno <helton_heleno@hotmail.com>
2026-05-05 23:32:59 -0300

    aula 14

---
+++ src/2. JavaScript Basico - para iniciantes/aula12/js/script.js
@@ -0,0 +1,17 @@
+alert('Com a nossa mensagem.');
+confirm('Deseja realmente apagar?');// chamada para um booleano true / false
+prompt('Digite o seu nome:') // chamada para o usuário responder algo
+const confirma = confirm('Realmente deseja apagar ?');
+
+let num1 = prompt('Digite um numero');
+let num2 = prompt('Digite outro numero');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado;
+
+resultado = num1 + num2;
+
+alert(`O resultado da sua conta foi:  ${resultado}`);
+alert(`O resultado da sua conta foi:  ${num1 + num2}`);
